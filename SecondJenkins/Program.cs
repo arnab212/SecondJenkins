fccdd69c0086f2b7cf5555ad2f37922bfe71631f
arnab212 <70711617+arnab212@users.noreply.github.com>
2021-07-29 11:21:37 +0530

    push

--- SecondJenkins/Program.cs
+++ SecondJenkins/Program.cs
@@ -7,6 +7,7 @@ namespace SecondJenkins
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Second try");
         }
     }
 }
